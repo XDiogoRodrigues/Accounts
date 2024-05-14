cf2a0bd9ed5d8b581658e21673a9ba5272941dfb
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-05-14 17:58:38 -0300

    Implementando a função responsável para exibir o menu

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const chalk = require("chalk");
-const inquirer = requeire("inquirer");
+const inquirer = require("inquirer");
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você gostaria de fazer?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then()
+    .catch((error) => console.log(error));
+}
