278490180b70df78c0ea8f8354fc094e0159dbf8
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-05-14 18:37:48 -0300

    Implementada a função responsável por criar e verificar a conta

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const chalk = require("chalk");
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 function operation() {
   inquirer
@@ -30,4 +31,42 @@ function operation() {
 function createAccount() {
   console.log(chalk.bgGreen.black("Parabêns por escolher o nosso banco!"));
   console.log(chalk.green("Defina as opções da sua conta a seguir"));
+  builderAccount();
+}
+
+function builderAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para a sua conta:",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Está conta já existe, escolha outra nome!")
+        );
+        builderAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      console.log(chalk.green("Parabêns, sua conta foi criada!"));
+      operation();
+    })
+    .catch((error) => console.log(error));
 }
