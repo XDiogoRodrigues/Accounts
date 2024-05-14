6a6bc29a837c0215b73e8d5c0d29c473d82ccfb9
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-05-14 18:12:25 -0300

    Começando a implementar a lógica das opções do menu, correspodendo a condição. também criado a função createAccount

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ function operation() {
         ],
       },
     ])
-    .then()
+    .then((answer) => {
+      const action = answer["action"];
+      if (action == "Criar Conta") {
+        createAccount();
+      }
+    })
     .catch((error) => console.log(error));
+}
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabêns por escolher o nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir"));
 }
